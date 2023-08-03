1796a84ebd6f9b1a7c31d624b8e30caabe41dff1
shiyuhang <1136742008@qq.com>
2023-08-03 14:18:36 +0800

    test

--- app/api/serverless-example/route.ts
+++ app/api/serverless-example/route.ts
@@ -3,9 +3,10 @@ import type { NextRequest } from 'next/server';
 import { connect } from '@shiyuhang0/serverless-js'
 
 export async function GET(request: NextRequest) {
-  const conn = connect({url: process.env.TIDB_URL,useIpAddress: true})
+  const conn = connect({url: process.env.TIDB_URL})
   const tx = await conn.begin()
-  await tx.execute('insert into  test.test values (10,"t")')
+  await tx.execute('select * from test.test')
+  // await tx.execute('insert into  test.test values (10,"t")')
   const result = await tx.execute('select * from test.test')
   await tx.rollback()
   return NextResponse.json(
