51232c0217eb32a20ae9413bec8bde45228c3f8b
shiyuhang <1136742008@qq.com>
2023-08-03 14:20:35 +0800

    test

--- app/api/serverless-example/route.ts
+++ app/api/serverless-example/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { connect } from '@shiyuhang0/serverless-js'
+export const runtime = 'edge'
 
 export async function GET(request: NextRequest) {
   const conn = connect({url: process.env.TIDB_URL})
