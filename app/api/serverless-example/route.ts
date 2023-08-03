f14010dc080d34921e22a7d2d6b850a216600743
shiyuhang <1136742008@qq.com>
2023-08-03 14:10:29 +0800

    test

--- app/api/serverless-example/route.ts
+++ app/api/serverless-example/route.ts
@@ -1,12 +1,11 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { connect } from '@shiyuhang0/serverless'
+import { connect } from '@shiyuhang0/serverless-js'
 
-export const runtime = 'edge'
 export async function GET(request: NextRequest) {
-  const conn = connect({url: process.env.TIDB_URL})
+  const conn = connect({url: process.env.TIDB_URL,useIpAddress: true})
   const tx = await conn.begin()
-  await tx.execute('insert into  test.test values (8,"t")')
+  await tx.execute('insert into  test.test values (10,"t")')
   const result = await tx.execute('select * from test.test')
   await tx.rollback()
   return NextResponse.json(
