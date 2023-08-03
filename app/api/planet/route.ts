b76d3fe4e35771e922b881b114599b0fef0fcdc2
shiyuhang <1136742008@qq.com>
2023-08-03 17:39:44 +0800

    test pt

--- app/api/planet/route.ts
+++ app/api/planet/route.ts
@@ -1,15 +1,20 @@
 import { connect } from '@planetscale/database'
+import {NextRequest, NextResponse} from "next/server";
+import {ipAddress} from "@vercel/edge";
 
-const config = {
-  url: process.env['DATABASE_URL'] || 'mysql://user:pass@host'
+export async function GET(request: NextRequest) {
+  const config = {
+    url: process.env['DATABASE_URL'] || 'mysql://user:pass@host'
+  }
+  const conn = connect(config)
+  const results = await conn.transaction(async (tx) => {
+    const r1 = await tx.execute('select * from test.user ')
+    const r2 = await tx.execute('select * from test.user ')
+    const r3 = await tx.execute('select * from test.user ')
+    return [r1, r2,r3]
+  })
+
+  return NextResponse.json(
+      {results}
+  );
 }
-
-const conn = connect(config)
-
-const results = await conn.transaction(async (tx) => {
-  const r1 = await tx.execute('select * from test.user ')
-  const r2 = await tx.execute('select * from test.user ')
-  const r3 = await tx.execute('select * from test.user ')
-  return [r1, r2,r3]
-})
-console.log(results)
