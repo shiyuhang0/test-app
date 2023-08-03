f7f86cf9fa7a96362e04b0a6fa53736d8881621f
shiyuhang <1136742008@qq.com>
2023-08-03 15:15:14 +0800

    recovery

--- app/api/serverless-example/route.ts
+++ app/api/serverless-example/route.ts
@@ -1,10 +1,13 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { connect } from '@shiyuhang0/serverless-js'
+import { ipAddress } from '@vercel/edge';
 export const runtime = 'edge'
 
 export async function GET(request: NextRequest) {
-  const conn = connect({url: process.env.TIDB_URL})
+  const ip = ipAddress(request) || 'unknown';
+  console.log(ip)
+  const conn = connect({url: process.env.TIDB_URL,useIpAddress: true})
   const tx = await conn.begin()
   await tx.execute('select * from test.test')
   // await tx.execute('insert into  test.test values (10,"t")')
