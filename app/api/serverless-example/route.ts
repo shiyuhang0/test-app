94582d5e55584ba8578cc60a217c6fb0b00a0111
shiyuhang <1136742008@qq.com>
2023-08-04 14:38:54 +0800

    test

--- app/api/serverless-example/route.ts
+++ app/api/serverless-example/route.ts
@@ -7,7 +7,7 @@ export const runtime = 'edge'
 export async function GET(request: NextRequest) {
   const ip = ipAddress(request) || 'unknown';
   console.log(ip)
-  const conn = connect({url: process.env.TIDB_URL,useIpAddress: true})
+  const conn = connect({url: process.env.TIDB_URL})
   const tx = await conn.begin()
   await tx.execute('select * from test.test')
   // await tx.execute('insert into  test.test values (10,"t")')
