c14e3d42edce1e7aae23a2c19f4de9e21de2931d
shiyuhang <1136742008@qq.com>
2023-08-09 11:01:18 +0800

    0.5.1

--- app/api/serverless-example/route.ts
+++ app/api/serverless-example/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { connect } from '@shiyuhang0/serverless'
+import { connect } from '@shiyuhang0/serverless-js'
 export const runtime = 'edge'
 
 export async function GET(request: NextRequest) {
