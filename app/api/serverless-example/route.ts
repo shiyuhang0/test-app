4031cb617be2141db9607d75e8967ebc9c0c7843
shiyuhang <1136742008@qq.com>
2023-08-09 13:56:30 +0800

    0.5.3

--- app/api/serverless-example/route.ts
+++ app/api/serverless-example/route.ts
@@ -8,7 +8,6 @@ export async function GET(request: NextRequest) {
   const conn = connect({url: process.env.TIDB_URL})
   const tx = await conn.begin()
   await tx.execute('select * from test.test')
-  // await tx.execute('insert into  test.test values (10,"t")')
   const result = await tx.execute('select * from test.test')
   await tx.rollback()
   return NextResponse.json(
